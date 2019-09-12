5a7b61a17f97ce4ca62d2cdfe7d03f9f840726dd
Bernhard Schmitt <schmitt@sitegeist.de>
2019-09-12 13:20:59 +0200

    Provide means to reset content dimensions and set site domains in tests

--- Tests/Behaviour/Bootstrap/ObjectsTrait.php
+++ Tests/Behaviour/Bootstrap/ObjectsTrait.php
@@ -6,9 +6,16 @@ declare(strict_types=1);
  */
 
 use Behat\Gherkin\Node\TableNode;
+use Doctrine\Common\Collections\ArrayCollection;
+use Neos\ContentRepository\Domain\Repository\ContentDimensionRepository;
+use Neos\ContentRepository\Domain\Service\ContentDimensionPresetSourceInterface;
+use Neos\Flow\Http\Uri;
 use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
+use Neos\Flow\Security\Context;
+use Neos\Neos\Domain\Model\Domain;
 use Neos\Neos\Domain\Model\Site;
+use Neos\Neos\Domain\Repository\DomainRepository;
 use Neos\Neos\Domain\Repository\SiteRepository;
 
 /**
@@ -16,6 +23,20 @@ use Neos\Neos\Domain\Repository\SiteRepository;
  */
 trait ObjectsTrait
 {
+    /**
+     * @Given /^I have no content dimensions$/
+     */
+    public function iHaveNoContentDimensions()
+    {
+        $dimensions = [];
+
+        $contentDimensionRepository = $this->getObjectManager()->get(ContentDimensionRepository::class);
+        $contentDimensionRepository->setDimensionsConfiguration($dimensions);
+
+        $contentDimensionPresetSource = $this->getObjectManager()->get(ContentDimensionPresetSourceInterface::class);
+        $contentDimensionPresetSource->setConfiguration($dimensions);
+    }
+
     /**
      * @Given /^I have the following sites:$/
      * @param TableNode $siteProperties
@@ -26,10 +47,21 @@ trait ObjectsTrait
         /** @var SiteRepository $siteRepository */
         $siteRepository = $this->getObjectManager()->get(SiteRepository::class);
 
+        /** @var DomainRepository $domainRepository */
+        $domainRepository = $this->getObjectManager()->get(DomainRepository::class);
+
         foreach ($siteProperties->getHash() as $row) {
             $site = new Site($row['nodeName']);
             $site->setName($row['name'] ?? '');
             $site->setSiteResourcesPackageKey($row['siteResourcesPackageKey'] ?? '');
+            if (isset($row['domain'])) {
+                $uri = new Uri($row['domain']);
+                $domain = new Domain();
+                $domain->setHostname($uri->getHost());
+                $domain->setScheme($uri->getScheme());
+                $domain->setSite($site);
+                $domainRepository->add($domain);
+            }
             $siteRepository->add($site);
         }
 
